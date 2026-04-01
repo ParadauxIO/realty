ed64bf19e1da02177c7fa87b60bd86cf557d233c
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2026-04-01 13:10:02 +1100

    Fix relocations

--- realty-paper-plan-extension/build.gradle.kts
+++ realty-paper-plan-extension/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 dependencies {
-    implementation(project(":realty-common"))
+    compileOnly(project(":realty-common"))
     compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
     compileOnly("com.github.plan-player-analytics:Plan:5.7.3306")
     compileOnly("org.jetbrains:annotations:26.0.2-1")
@@ -18,9 +18,19 @@ dependencies {
 
 tasks {
     shadowJar {
-        val base = "io.github.md5sha256.realty.plan.libraries"
+        val base = "io.github.md5sha256.realty.libraries"
         relocate("org.jetbrains.annotations", "${base}.org.jetbrains.annotations")
         relocate("org.intellij.lang", "${base}.org.intellij.lang")
+        relocate("org.mariadb", "${base}.org.mariadb")
+        relocate("org.mybatis", "${base}.org.mybatis")
+        relocate("org.spongepowered", "${base}.org.spongepowered")
+        relocate("org.yaml", "${base}.org.yaml")
+        relocate("io.leangen.geantyref", "${base}.io.leangen.geantyref")
+        relocate("org.apache.ibatis", "${base}.org.apache.ibatis")
+        relocate("org.jetbrains.annotations", "${base}.org.jetbrains.annotations")
+        relocate("org.intellij.lang", "${base}.org.intellij.lang")
+        relocate("net.kyori.option", "${base}.net.kyori.option")
+        relocate("org.incendo.cloud", "${base}.org.incendo.cloud")
     }
 
     processResources {
